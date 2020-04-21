1593101967070155d768b544f74f5cb800367d8a
doctafaustus <clowd150@gmail.com>
2020-04-21 13:32:48 -0500

    Add Optmizely test page

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const favicon = require('serve-favicon');
 
-// Globals
-const port = process.env.PORT || 3000;
 
 // Initiate Express
 const app = express();
 app.use(favicon(`${__dirname}/favicon.ico`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(port, function(req, res) {
+app.get('/optimizely-test', (req, res) => {
+	res.sendFile(`${__dirname}/optimizely-test.html`);
+});
+
+app.listen(process.env.PORT || 3000, (req, res) => {
 	console.log('App listening on port 3000');
 });
